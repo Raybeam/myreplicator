cc1660a04d2cfa5f6132ad7a4977b17c5afca383
Sasan <siesonz@gmail.com>
2012-11-24 00:02:51 -0800

    Threading with clean ups completed, download works

--- lib/transporter/parallelizer.rb
+++ lib/transporter/parallelizer.rb
@@ -12,37 +12,41 @@ module Myreplicator
       @max_threads = options[:max_threads].nil? ? 10 : options[:max_threads]
     end
 
+    ##
+    # Runs while there are jobs in the queue
+    # Waits for a second and checks for available threads
+    # Exits when all jobs are allocated in threads
+    ##
     def run
       while @queue.size > 0
         if @threads.size <= @max_threads
           @threads << Thread.new(@queue.pop) do |proc|
-            Thread.current[:status] = 'running'
+            Thread.current[:status] = 'running' # Manually Set Thread state for Checks
             Transporter.new.instance_exec(proc[:params], &proc[:block])
-            Thread.current[:status] = 'done'
+            Thread.current[:status] = 'done' 
           end
+        else
+          sleep 1
         end
       end 
-      
-      # done = false
-      Thread.new do 
-        counter =0
-        while(counter < 5)
-          puts @threads.size
-          @threads.each do |t|
-            puts t[:status]
-          end
-          counter += 1
-          sleep 2
-        end
-      end
 
-      Kernel.p @threads
+      manage_threads
     end
-
-    def check_threads
+    
+    ##
+    # Clears dead threads, 
+    # frees thread pool for more jobs
+    # Exits when no more threads are left
+    ##
+    def manage_threads
       Thread.new do 
-        @threads.each do |t|
-          
+        while(@threads.size > 0)
+          done = []
+          @threads.each do |t|
+            done << t if t[:status] == "done"
+          end
+          done.each{|d| @threads.delete(d)} # Clear dead threads
+          sleep 2 # Wait for more threads to spawn
         end
       end
     end
