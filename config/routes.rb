a535e3f75e597ec199e656828d908edf15d1ca83
Sasan <siesonz@gmail.com>
2012-12-03 15:54:12 -0800

    released 13, uses 3.2+

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Myreplicator::Engine.routes.draw do
-  # get "myreplicator" => "myreplicator/home#index" , :as => :myreplicator
   resources :exports
   root :to => "home#index"
   match '/errors', :to => "home#errors", :as => 'errors'
