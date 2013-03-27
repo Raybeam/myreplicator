3538489bad000afe3cd1edc544c55c4407fe03a5
landn <lnguyen@raybeam.com>
2013-03-26 21:51:38 -0700

    Adding sweeper to clear dead or stuck process

---
+++ app/models/myreplicator/sweeper.rb
@@ -0,0 +1,18 @@
+require "log"
+
+module Myreplicator
+  class Sweeper
+   
+    @queue = :myreplicator_sweeper # Provided for Resque
+    
+    ##
+    # Main method provided for resque
+    # Reconnection provided for resque workers
+    ##
+    def self.perform
+      Myreplicator::Log.clear_deads
+      Myreplicator::Log.clear_stucks
+    end
+    
+  end
+end
